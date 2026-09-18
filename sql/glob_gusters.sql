f3f4ffd89797defe22e0efb98b9736cbcf9b84f1
jonathan david <jonathandavidesquivelbejarangm@gmail.com>
2026-09-18 12:02:21 -0500

    fix: Corregir definición de claves primarias en tablas para uso de AUTO_INCREMENT

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -10,10 +10,62 @@ USE `glob_gusters`;
 -- DROP DATABASE IF EXISTS `glob_gusters`;
 
 
-CREATE TABLE Persons (
-    PersonID int PRIMARY KEY,
-    LastName varchar(255) NOT NULL,
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
+-- 1. Tablas independientes
+CREATE TABLE Estado (
+    ID_Estado int AUTO_INCREMENT PRIMARY KEY,
+    Nombre varchar(100)
+); 
+
+CREATE TABLE Productora (
+    ID_Productora int AUTO_INCREMENT PRIMARY KEY,
+    Nombre varchar(100)
+);
+
+CREATE TABLE Cliente (
+    ID_Cliente int AUTO_INCREMENT PRIMARY KEY,
+    Nombre varchar(100),
+    Direccion varchar(100),
+    Telefono INT
+); 
+
+CREATE TABLE Director (
+    ID_Director int AUTO_INCREMENT PRIMARY KEY,
+    ID_Nacionalidad int,
+    Nombre varchar(100),
+    FOREIGN KEY (ID_Nacionalidad) REFERENCES Nacionalidad(ID_Nacionalidad)
+);
+
+CREATE TABLE Actor (
+    ID_Actor int AUTO_INCREMENT PRIMARY KEY,
+    ID_Nacionalidad int,
+    Nombre varchar(100),
+    Sexo varchar(100),
+    FOREIGN KEY (ID_Nacionalidad) REFERENCES Nacionalidad(ID_Nacionalidad)
+);
+
+CREATE TABLE Pelicula (
+    ID_Pelicula int AUTO_INCREMENT PRIMARY KEY,
+    Titulo VARCHAR(100),
+    ID_Productora int,
+    ID_Nacionalidad int,
+    ID_Director int,
+    FOREIGN KEY (ID_Productora) REFERENCES Productora(ID_Productora),
+    FOREIGN KEY (ID_Nacionalidad) REFERENCES Nacionalidad(ID_Nacionalidad),
+    FOREIGN KEY (ID_Director) REFERENCES Director(ID_Director)
+); 
+
+CREATE TABLE Ejemplar (
+    ID_Ejemplar int AUTO_INCREMENT PRIMARY KEY,
+    Estado_ID int,
+    ID_Pelicula int,
+    FOREIGN KEY (Estado_ID) REFERENCES Estado(ID_Estado),
+    FOREIGN KEY (ID_Pelicula) REFERENCES Pelicula(ID_Pelicula)
+);
+
+CREATE TABLE Renta (
+    ID_Renta int AUTO_INCREMENT PRIMARY KEY,
+    Inicia DATE,
+    Finaliza DATE,
+    ID_Cliente int,
+    FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente)
 );
